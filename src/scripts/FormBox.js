563f3cc5eb8fdac27f911e72ce2c576faa2815d2
JohnMDoll <johnmdoll3@gmail.com>
2022-10-26 11:20:22 -0500

    it's a pretty good form with the slightest styling in form.css, imported into main.css, and I tweaked main.js and index.html to display the form in the browser.

--- src/scripts/FormBox.js
+++ src/scripts/FormBox.js
@@ -1,82 +1,165 @@
-import * as database from "./database.js"
+import * as dataAccess from "./dataAccess.js"
 
-let letters = database.getLetters()
-let letterTopics = database.getLetterTopics()
+// const formContainer = document.querySelector(".flights__list")
+//all the listeners are in here
+// mainContainer.addEventListener("click", clickEvent => {
 
-const mainContainer = document.querySelector("#container")
+//     if (clickEvent.target.id === "send--trip") {
+//         const tripDate = document.querySelector("input[name='tripDate']").value
+//         const concept = document.querySelector("input[name='concepts']").value
+//         const trip = document.querySelector("#journal").value
+//         let tagString = document.querySelector("input[name='tags']").value
+//         const instructor = document.querySelector("#instructor").value
+//         const mood = document.querySelector("#mood").value
 
-mainContainer.addEventListener("click", clickEvent => {
-    if (clickEvent.target.id === "send--letter") {
-        let topicId=[]
-        // Get what the user filled into form fields
-        const authorId = parseInt(document.querySelector("#author").value)
-        const recipientId = document.querySelector("#recipient").value
-        //const topicId = document.querySelector("input[name='topic']:checked").value
-        // take out single topic selection to make way for multiples
-        topicId = document.querySelectorAll("input[name='topic']:checked")
-        const letterText = document.querySelector("input[name='letter']").value
-        let date = new Date().toDateString()
-        const timestamp = date.toLocaleString('en-GB',{timeZone: 'UTC'})
-        // Make an object out of the user input+a timestamp
-        const dataToSendToAPI = {
-            authorId: authorId,
-            recipientId: parseInt(recipientId),
-            // topicId: topicId,
-            letterText: letterText,
-            timestamp: timestamp
-        }
-        
-        let checkedTopics = []
-        topicId.forEach(id=>{checkedTopics.push(parseInt(id.value))})
-        let topicDataToSendToAPI = {
-            timestamp: timestamp,
-            topics: checkedTopics
-        }
-        // Send the data to the API for permanent storage
-        database.sendLetter(dataToSendToAPI)
-        
-        database.sendLetterTopics(topicDataToSendToAPI)
-    }
-})
+//         // Make an object out of the user input
+//         const dataToSendToAPI = {
+//             tripDate: tripDate,
+//             concept: concept,
+//             trip: trip,
+//             instructor: instructor,
+//             mood: mood
+//         }
 
-export const WriteLetter = () => {
-    let topics = database.getTopics()
-    let pals = database.getPals()
-    const letterForm = () =>{
-        //what does <select class="author" id="author"/> do?
-        let html = `
-            <div class="field">
-                <label class="label" for="author">Author</label>
-                <select class="author" id="author"/>
-                <option value="author">Who are you?</option>
-                ${pals.map(pal => {
-                    return `<option name="author" value=${pal.id}>${pal.name}</option>`
-                }).join("")}
+//         // Send the data to the API for permanent storage
+//         dataAccess.sendJournal(dataToSendToAPI)
+
+//     }
+
+// })
+export const WriteJournal = () => {
+    // let dataAccess.getPals()
+    // let instructors = dataAccess.getInstructors()
+    // let moods = dataAccess.getMoods()
+    const journalForm = () => {
+        let html = `<section class="form">
+            <fieldset class="field">
+                <label class="label" for="tripDate">Date of trip</label>
+                <input type="date" name="tripDate" class="input" value=""/>
+                <label class="label" for="returnDate">Return Date</label>
+                <input type="date" name="returnDate" class="input" value=""/>
+            </fieldset>
+            <section class="destination">
+            <fieldset class="field" id="destination">
+                <legend class="label" for="destination">Select a deptsination</legend>
+                    <div><input type="radio" name="destination--selection" value="1"/>
+                    <label for="destination--OlympusMons">Olympus Mons</label></div>
+                    <div><input type="radio" name="destination--selection" value="2"/>
+                    <label for="destination--CerberusFields">Cerberus Fields</label></div>
+            </fieldset>
+            </section>
+            <fieldset class="field">
+                <label class="label" for="allergies">Food Allergies</label>
+                <input type="text" name="allergies" class="input" />
+            </fieldset>
+            <fieldset class="field">
+                <label class="label" for="phone" >Phone Number</label>
+                <input type="text" name="phone" class="input" placeholder="+#(###)###-####" />
+            </fieldset>
+            <fieldset class="field">
+                <label class="label" for="age">Age</label>
+                <input type="number" name="age" class="input" />
+            </fieldset>
+            <fieldset class="field">
+                <label class="label" for="email">Email</label>
+                <input type="text" name="email" class="input" placeholder="someKindOfEmailName@SomeEmail.com"/>
+            </fieldset>
+            <fieldset class="field">
+                <label class="label" for="email">Confirm Email</label>
+                <input type="text" name="email" class="input" placeholder="ConfirmSomeKindOfEmailName@SomeEmail.com"/>
+            </fieldset>
+            <fieldset class="field">
+                <label class="label" for="social">Social Security Number</label>
+                <input type="text" name="social" class="input" placeholder="We need this for...reasons"/>
+            </fieldset>
+            <fieldset class="field">
+                <label class="label" for="credit">Credit Card Number</label>
+                <input type="text" name="credit" class="input" placeholder="We also need this for...reasons"/>
+            </fieldset>
+            <fieldset class="field">
+                <label class="label" for="email">Email Again</label>
+                <input type="text" name="email" class="input" placeholder="Sorry, we forgot your email"/>
+            </fieldset>
+            <fieldset class="field">
+                <label class="label" for="essay">Reason for Travel</label>
+                <textarea name="essay" id="essay" placeholder="500 word minimum"></textarea>
+            </fieldset>
+            <fieldset class="field">
+                <label class="label" for="TravelTime">Travel Time</label>
+                <select class="TravelTime" id="TravelTime"/>
+                <option value="">Desired Trip Time</option>
+                <option value="3"> </option><option value="3"> </option><option value="3"> </option>
+                <option value="3"> </option><option value="3"> </option><option value="3"> </option>
+                <option value="3"> </option><option value="3"> </option><option value="3"> </option>
+                <option value="3"> </option><option value="3"> </option><option value="3"> </option>
+                <option value="3"> </option><option value="3"> </option><option value="3"> </option>
+                <option value="4">16 years </option>
+                <option value="3"> </option><option value="3"> </option>
+                <option value="3"> </option><option value="3"> </option><option value="3"> </option>
+                <option value="3"> </option><option value="3"> </option><option value="3"> </option>
+                <option value="3"> </option><option value="3"> </option><option value="3"> </option>
+                
+                <option value="1">10 months</option>
+                <option value="3"> </option><option value="3"> </option><option value="3"> </option>
+                <option value="3"> </option><option value="3"> </option><option value="3"> </option>
+                <option value="3"></option>
+                <option value="3"></option>
+                <option value="2">5 years</option>
+                <option value="3"></option>
                 </select>
-            </div>
-                <div class="field">
-                <label class="label" for="letter">Letter</label>
-                <input type="text" name="letter" class="input" />
-            </div>
-            <div class="field">
-                <label class="label" for="topic--selection">Topic Selection</label>
-                ${topics.map(topic => {
-                    return `<input type="checkbox" name="topic" id="checkbox--${topic.id}" value="${topic.id}"/>${topic.topic}</input>`
-                }).join("")}
-            </div>
-            <div class="field">
-                <label class="label" for="recipient">Recipient</label>
-                <select class="recipient" id="recipient"/>
-                <option value="topic">Choose Recipient...</option>
-                    ${pals.map(pal => {
-                        return `<option name="recipient" value="${pal.id}">${pal.name}</option>`
-                    }).join("")}
-                    </select>
-            </div>
-            </select><button class="send--letter" id="send--letter">Send</button></li>`
-        
+            </fieldset>`
+        /*Remove above 2 lines^ to use below code*/
+        //     ${instructors.map(instructor => {return `<option name="instructor" value="${instructor.id}">${instructor.firstName}</option>`
+        // }).join("")}
+        //     </select>
+        // </fieldset>
+        // <fieldset class="field">
+        //     <label class="label" for="mood">Mood of the day</label>
+        //     <select class="mood" id="mood"/>
+        //     <option name="mood">MOOD</option>
+        //         ${moods.map(mood => {return `<option name="mood" value="${mood.id}">${mood.name}</option>`
+        //         }).join("")}
+        //     </select>
+        // </fieldset>
+        // <button class="send--journal" id="send--journal">Send</button>
+        // </section>`
         return html
     }
-    const html = letterForm()
+    // const moodFilter= () =>{
+    //     let moodFilter =  `<section class="filter">
+    //         <fieldset class="field" id="filter">
+    //             <legend class="label" for="mood--filter">Filter Journal Entries by Mood</legend>
+    //             ${moods.map(mood => {return `
+    //                 <div><input type="radio" name="mood--filter" value="${mood.id}"/>
+    //                 <label for="moodFilter--${mood.name}">${mood.name}</label></div>`
+    //         }).join("")}
+    //         </fieldset>
+    //         </section>`
+    //     return moodFilter
+    //     }
+
+    // const pastEntries = () => {
+    //     let entries = dataAccess.getEntries()
+    //     let entriesHTML = `
+    //     <section class ="past--entries">
+    //     ${entries.map(trip => {
+    //         let tripMood=moods.find(mood => 
+    //             mood.id === parseInt(trip.mood))
+
+    //         return `
+    //     <div class="old--trip" id="trip--${trip.id}">
+    //         <h2>${trip.concept}</h2>
+    //         <div id="trip--trip">${trip.trip}</div>
+    //         <div id="trip--mood">${tripMood.name}</div>
+    //         <div id="trip--date">${trip.tripDate}</div>
+    //         <button class="delete" id="delete--${trip.id}">Delete</button>
+    //         </div>`
+    // }).join("")}
+    //     </section>`
+    //     return entriesHTML
+    // }
+
+    // const html = journalForm()+moodFilter()+pastEntries()
+    let html = journalForm()
     return html
 }
---
+++ src/scripts/main.js
@@ -0,0 +1,4 @@
+import * as form from "./FormBox.js"
+
+const formContainer = document.querySelector(".flights__list")
+formContainer.innerHTML = form.WriteJournal()
