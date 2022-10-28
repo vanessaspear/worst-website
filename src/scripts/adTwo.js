8d22e6f4e78d5d8393a73609d2a1cfc222267c78
Hazel Preza <hazel.d.preza@gmail.com>
2022-10-28 16:17:13 -0500

    added ads for the Mars Red Light District and Blips and Chitz

---
+++ src/scripts/adTwo.js
@@ -0,0 +1,17 @@
+export const adTwo = () => {
+    return `
+ <a href="https://www.youtube.com/watch?v=8RxDVdP2TZ8"><img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/blips-and-chitz-world-faleshia-bandriya.jpg" width="250px"></a>
+<div class="blipsAndChitzAd"> You feelin' lucky? Come to Blips and Chitz!<br><br>
+
+Play games!*<br><br>
+
+Win prizes!<br><br>
+
+We are not responsible if you are a fugitive on the run from the Intergalactic Federation and are found
+and killed by  bounty hunters on our premesis. What happens at Blips and Chitz stays at Blips and Chitz.<br><br>
+
+*If you die in the games, you die in real life. Don't die in the games.
+
+</div>
+    `
+}
